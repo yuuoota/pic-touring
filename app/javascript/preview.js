97ad60899a7d1f3ae3f8e1274c644810d2bfde40
yuuoota <93074354+yuuoota@users.noreply.github.com>
2022-02-23 01:06:59 +0900

    画像選択時のプレビュー表示の実装

---
+++ app/javascript/preview.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', function(){
+  const postForm = document.getElementById('new_post');
+  const previewList = document.getElementById('previews');
+
+  if (!postForm) return null;
+
+  const fileField = document.querySelector('input[type="file"][name="post[image]"]');
+  fileField.addEventListener('change', function(e){
+    
+    const alreadyPreview = document.querySelector('.preview');
+    if (alreadyPreview) {
+      alreadyPreview.remove();
+    };
+
+    const file = e.target.files[0];
+    const blob = window.URL.createObjectURL(file);
+
+    const previewWrapper = document.createElement('div');
+    previewWrapper.setAttribute('class', 'preview');
+    const previewImage = document.createElement('img');
+    previewImage.setAttribute('class', 'preview-image');
+    previewImage.setAttribute('src', blob);
+
+    previewWrapper.appendChild(previewImage);
+    previewList.appendChild(previewWrapper);
+  });
+});
