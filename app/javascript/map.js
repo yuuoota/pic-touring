3747f25405fad6ae39e4e5b29579c79738812396
yuuoota <93074354+yuuoota@users.noreply.github.com>
2022-03-29 01:53:02 +0900

    map.jsの編集

--- app/javascript/map.js
+++ app/javascript/map.js
@@ -3,14 +3,12 @@ document.addEventListener('DOMContentLoaded', function(){
   const mapPlace = document.getElementById('map');
   // 詳細ページのフォームがないならここで終了
   if (!mapPlace) return null;
-  // Create the script tag, set the appropriate attributes
   var script = document.createElement('script');
   script.src = `https://maps.googleapis.com/maps/api/js?key=${process.env.MAPS_API_KEY}&callback=initMap`;
   script.async = true;
-  // Append the 'script' element to 'head'
+
   document.head.appendChild(script);
 
-  // Attach your callback function to the `window` object
   window.initMap = function() {
     var map;
     var marker = [];
@@ -31,7 +29,7 @@ document.addEventListener('DOMContentLoaded', function(){
           position:  latLng,
           map: map
         });
-        infoWindow[i] = new google.maps.InfoWindow({ // 吹き出しの追加
+        infoWindow[i] = new google.maps.InfoWindow({
           content: '<div class="info_window">' + "写真の場所" + '</div>'
         }); 
         if(i == 0 && marker[i]){
@@ -39,7 +37,7 @@ document.addEventListener('DOMContentLoaded', function(){
           currentInfoWindow = infoWindow[i];
         };
         changeEvent(i);
-        markerEvent(i); // マーカーにクリックイベントを追加
+        markerEvent(i);
         count += 1;
       };
     };
@@ -65,7 +63,7 @@ document.addEventListener('DOMContentLoaded', function(){
                                 map: map // マーカーを立てる地図を指定
                             });
                         } else { // 失敗した場合
-                        }//if文の終了。ifは文なので;はいらない
+                        }
                         if (--cRef <= 0) {
                             callback();//全て取得できたらafter_geocode実行
                         }
@@ -90,25 +88,24 @@ document.addEventListener('DOMContentLoaded', function(){
       });
     };  
   
-    // マーカーにクリックイベントを追加
     function markerEvent(i) {
-      marker[i].addListener('click', function() { // マーカーをクリックしたとき
+      marker[i].addListener('click', function() {
         if (currentInfoWindow) {
           currentInfoWindow.close();
         };
-        infoWindow[i].open(map, marker[i]); // 吹き出しの表示
+        infoWindow[i].open(map, marker[i]);
         $('.slider').slick('slickGoTo', i, false);
         currentInfoWindow = infoWindow[i];
       });
     };
 
     function after_geocode(){
-      myLatLng = latLng[0];//最初の住所を地図の中心点に設定
+      myLatLng = latLng[0];
       var opt = {
-          center: myLatLng, // 地図の中心を指定
-          zoom: 10 // 地図のズームを指定
-      };//地図作成のオプションのうちcenterとzoomは必須
-      map.setOptions(opt);///オプションをmapにセット
-    };//function after_geocode終了
+          center: myLatLng,
+          zoom: 10
+      };
+      map.setOptions(opt);
+    };
   };
 });
